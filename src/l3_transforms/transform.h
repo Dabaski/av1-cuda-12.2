9b4fa263684e2c87731fffcf4a0ae732f930e80d
Dabaski <dabaski@users.noreply.github.com>
2026-08-16 10:23:36 -0400

    T8: retire float DCT/ADST API and CuSources; integer pipeline is the single source

--- src/l3_transforms/transform.h
+++ src/l3_transforms/transform.h
@@ -26,12 +26,4 @@ void fwdTxfm2d4x4(const std::int16_t* input, std::int32_t* output, std::uint32_t
 
 std::string fwdTxfmCuSource();
 
-void dct2Forward4x4(const float in[16], float out[16]);
-
-void adst4Forward4x4(const float in[16], float out[16]);
-
-std::string dct2Forward4x4CuSource();
-
-std::string adst4Forward4x4CuSource();
-
 }  // namespace transforms
